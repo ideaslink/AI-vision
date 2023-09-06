00bc73ab42aa81857e08db7ff3967fda1fada1dc
johnlauren <aplusview@gmail.com>
2023-09-05 20:18:02 -0400

    maintenance changes

--- image_analysis_azure/secretvars.py
+++ image_analysis_azure/secretvars.py
@@ -6,19 +6,13 @@
     azure computer vision - Key and endpoint
 '''
 
-COG_VISION_KEY = 'a5cc0af921ac4e61980f60518a293c67'
-COG_VISION_ENDPOINT = 'https://vcivision.cognitiveservices.azure.com/'
-
-# COG_VISION_KEY = '<Paste your key here>'
-# COG_VISION_ENDPOINT = '<Paste your endpoint here>'
+COG_VISION_KEY = '<Paste your key here>'
+COG_VISION_ENDPOINT = '<Paste your endpoint here>'
 
 
 '''
     image for test
 '''
 
-
-IMAGE_URL_1 = 'https://vcistorage.blob.core.windows.net/demo/trump_td.jpg'
-
-# IMAGE_URL_1 = '<paste your image url>'
+IMAGE_URL_1 = '<paste your image url>'
 
